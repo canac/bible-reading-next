87fb8baa8959643877d955307327f4caec94aedd
Caleb Cox <canac@users.noreply.github.com>
2021-11-03 16:49:14 -0500

    Add server-side rendering

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,24 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import { withUrqlClient } from 'next-urql';
 import React from 'react';
-import { Context, createClient } from 'urql';
 import Journey from '../components/Journey';
 import styles from '../styles/Index.module.css';
 
-const client = createClient({
-  url: 'http://localhost:8081/v1/graphql',
-});
-
 const Index: NextPage = () => {
   return (
-    <Context.Provider value={client}>
-      <div className={styles.app}>
-        <Head>
-          <title>Bible Reading</title>
-        </Head>
-        <Journey />
-      </div>
-    </Context.Provider>
+    <div className={styles.app}>
+      <Head>
+        <title>Bible Reading</title>
+      </Head>
+      <Journey />
+    </div>
   );
 };
 
-export default Index;
+export default withUrqlClient(
+  () => ({
+    url: 'http://localhost:8081/v1/graphql',
+  }),
+  { ssr: true },
+)(Index);
